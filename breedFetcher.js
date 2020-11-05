8800da0ba4d6ebb84981325936eb8fd3b3c68997
Samantha Fok <samantha.fok@hotmail.com>
2020-11-05 23:50:21 +0000

    Returned error msg if error and if breed not found

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,10 +1,21 @@
 const request = require('request');
-const catInput = process.argv[2]
+const catInput = process.argv[2];
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${catInput}`,(error, response, body) => {
-  console.log('error:', error);
-  console.log('response:', response && response.statusCode);
+  if (error) {
+    console.log('Error:', error)
+    return;
+  }
+  
   const data = JSON.parse(body);
-  //console.log(data);
-  console.log(data[0].description);
+  const breed = data[0];
+  //How to access other keys in the data object instead of indexing.
+  const {energy_level, grooming: hygiene, stranger_friendly, description} = breed;
+  const grooming = hygiene;
+  if (data.length === 0) {
+    console.log('Sorry! Your requested breed was not found.');
+  } else {
+    console.log(description);
+  }
+  //console.log('response:', response && response.statusCode);
 });
