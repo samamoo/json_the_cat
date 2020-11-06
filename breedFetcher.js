6295258a34799024b0c74ad1128af110394d16ec
Samantha Fok <samantha.fok@hotmail.com>
2020-11-06 01:09:10 +0000

    Refactored the function with callbacks

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,27 @@
 const request = require('request');
-const catInput = process.argv[2];
+const breedName = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${catInput}`,(error, response, body) => {
-  if (error) {
-    console.log('Error:', error)
-    return;
-  }
-  
-  const data = JSON.parse(body);
-  const breed = data[0];
-  //How to access other keys in the data object instead of indexing.
-  const {energy_level, grooming: hygiene, stranger_friendly, description} = breed;
-  const grooming = hygiene;
-  if (data.length === 0) {
-    console.log('Sorry! Your requested breed was not found.');
-  } else {
-    console.log(description);
-  }
-  //console.log('response:', response && response.statusCode);
-});
+
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,(error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    const data = JSON.parse(body);
+    const breed = data[0];
+    //How to access other keys in the data object instead of indexing.
+    // const {energy_level, grooming: hygiene, stranger_friendly, description} = breed;
+    // const grooming = hygiene;
+    if (data.length === 0) {
+      callback('Sorry! Your requested breed was not found.', null);
+    } else {
+      callback(null, breed.description);
+    }
+    //console.log('response:', response && response.statusCode);
+  });
+};
+
+// fetchBreedDescription(breedName)
+
+module.exports = {fetchBreedDescription};
